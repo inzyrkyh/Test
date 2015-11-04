91ce259f7190b6a85fad1107ee7198232898e2e1
inzyrkyh <randomhou@gmail.com>
2015-11-04 09:47:07 +0800

    增加style资源 增加splashActivity

---
+++ app/src/main/java/com/test/test/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.test.test;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static boolean splashLoaded = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        if (!splashLoaded) {
+            setContentView(R.layout.activity_splash);
+            int secondsDelayed = 1;
+            new Handler().postDelayed(new Runnable() {
+                public void run() {
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    finish();
+                }
+            }, secondsDelayed * 500);
+
+            splashLoaded = true;
+        }
+        else {
+            Intent goToMainActivity = new Intent(SplashActivity.this, MainActivity.class);
+            goToMainActivity.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+            startActivity(goToMainActivity);
+            finish();
+        }
+    }
+}
